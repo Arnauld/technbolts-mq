79e10b703d3f4b74818e9fb305eae58151ab11b6
Arnauld <arnauld.loyer@gmail.com>
2010-05-27 00:00:22 +0200

    add protocol buffer

---
+++ src/main/scala/org/technbolts/model/io/bdb/MsgDataAssembler.scala
@@ -0,0 +1,11 @@
+package org.technbolts.model.io.bdb
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: arnauld
+ * Date: 26 mai 2010
+ * Time: 21:38:08
+ * To change this template use File | Settings | File Templates.
+ */
+
+class MsgDataAssembler
